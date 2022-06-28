9963d81755521e6da11fd2dda9cbb738f8b0579f
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2022-06-27 21:16:26 -0300

    Implementar exercício 2

--- main.hs
+++ main.hs
@@ -7,6 +7,15 @@ maior4 x y z w
     | w >= x && w >= y && w >= z = w
     
 -- ex2
+converterNotaParaMencao :: Float -> String
+converterNotaParaMencao nota
+    | nota >= 9.0 && nota <= 10.0 = "SS"
+    | nota >= 7.0 && nota <= 8.9 = "MS"
+    | nota >= 5.0 && nota <= 6.9 = "MM"
+    | nota >= 3.0 && nota <= 4.9 = "MI"
+    | nota >= 0.1 && nota <= 2.9 = "II"
+    | nota == 0.0 = "SR"
+
 -- ex3
 -- ex4
 -- ex5
@@ -15,13 +24,15 @@ maior4 x y z w
 
 
 
-main = do
-    x <- getLine
-    y <- getLine
-    z <- getLine
-    w <- getLine    
-    let xAsInt = (read x :: Int)
-    let yAsInt = (read y :: Int)
-    let zAsInt = (read z :: Int)
-    let wAsInt = (read w :: Int)
-    print(maior4 xAsInt yAsInt zAsInt wAsInt)
+
+-- main = do
+    -- x <- getLine
+    -- y <- getLine
+    -- z <- getLine
+    -- w <- getLine    
+    -- let xAsInt = (read x :: Float)
+    -- let yAsInt = (read y :: Int)
+    -- let zAsInt = (read z :: Int)
+    -- let wAsInt = (read w :: Int)
+    -- print(maior4 xAsInt yAsInt zAsInt wAsInt)
+    -- print(converterNotaParaMencao x)
